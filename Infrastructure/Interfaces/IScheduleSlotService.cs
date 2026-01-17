b1d6f6929a7c2d46d20ca83cac47647f67e76723
Umar Davlatov <u9884118@gmail.com>
2026-01-17 23:47:41 +0500

    v1

--- Infrastructure/Interfaces/IScheduleSlotService.cs
+++ Infrastructure/Interfaces/IScheduleSlotService.cs
@@ -2,8 +2,7 @@ public interface IScheduleSlotService
 {
     Task<Response<string>> AddAsync(ScheduleSlotInsertDto scheduleSlotInsScheduleSlotInsertDto);
     Task<List<ScheduleSlot>> GetAllSlots();
-    Task<Response<ScheduleSlot>> GetSlotById(int slotId);
-    Task<Response<ScheduleSlot>> GetSlotByDoctorId(int doctorId);
+    Task<Response<ScheduleSlotDtoForJoinDoctors>> GetSlotByDoctorId(int doctorId);
     Task<Response<string>> UpdateAsync(ScheduleSlotUpdateDto scheduleSlotUpdateDto);
     Task<Response<string>> SoftDelete(int slotId);
 }
